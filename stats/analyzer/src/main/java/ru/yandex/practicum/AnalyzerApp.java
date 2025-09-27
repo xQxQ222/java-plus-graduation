5cfffb5aaded63d55c3c5ae359e59f8ab7de9713
Ivan Korolkov <nerounxqxq@gmail.com>
2025-09-28 01:56:54 +0400

    add schema to analyzer

--- stats/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApp.java
+++ stats/analyzer/src/main/java/ru/yandex/practicum/AnalyzerApp.java
@@ -2,10 +2,26 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.yandex.practicum.processor.ActionProcessor;
+import ru.yandex.practicum.processor.SimilarityProcessor;
 
 @SpringBootApplication
 public class AnalyzerApp {
     public static void main(String[] args) {
-        SpringApplication.run(AnalyzerApp.class, args);
+        ConfigurableApplicationContext context =
+                SpringApplication.run(AnalyzerApp.class, args);
+        final ActionProcessor actionProcessor =
+                context.getBean(ActionProcessor.class);
+        SimilarityProcessor similarityProcessor =
+                context.getBean(SimilarityProcessor.class);
+
+        Thread hubEventsThread = new Thread(actionProcessor);
+        hubEventsThread.setName("UserActionProcessorThread");
+        hubEventsThread.start();
+
+        Thread snapshotThread = new Thread(similarityProcessor);
+        snapshotThread.setName("SimilarityProcessorThread");
+        snapshotThread.start();
     }
 }
